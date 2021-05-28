9dfeaaa204c4f6eb2b691acf958529adb729fea7
Victor Ferraz <cashrox@gmail.com>
2021-05-27 21:36:57 -0300

    add error video detail

--- src/pages/api/video/[videoId].ts
+++ src/pages/api/video/[videoId].ts
@@ -5,14 +5,18 @@ export default async (req, res) => {
   const { videoId } = req.query;
   const url = `https://www.youtube.com/watch?v=${videoId}`;
   const range = req.headers.range || 0;
-  const { start, end, total, contentLength} = await video.getVideoRange(url, range);
-  const headers = {
-    'Content-Range': `bytes ${start}-${end}/${total}`,
-    'Accept-Ranges': 'bytes',
-    'Content-Length': contentLength,
-    'Content-Type': 'video/mp4',
-  };
-  res.writeHead(206, headers);
-  const data = video.createStreams(url, start, end)
-  data.pipe(res);
+  try{
+    const { start, end, total, contentLength} = await video.getVideoRange(url, range);
+    const headers = {
+      'Content-Range': `bytes ${start}-${end}/${total}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': contentLength,
+      'Content-Type': 'video/mp4',
+    };
+    res.writeHead(206, headers);
+    const data = video.createStreams(url, start, end)
+    data.pipe(res);
+  } catch(e){
+    console.log(e);
+  }
 }
